ec29977f8f5cfa2616cd8c4530a15de39a23770a
GSingh2432002 <gsingh332211@gmail.com>
2024-02-24 11:23:17 +0530

    Update

---
+++ 02.DataTypes.js
@@ -0,0 +1,20 @@
+//Treat all JS Code as newer version
+"use strict"; 
+
+//We are using nodejs, not browser
+// alert(4+4)
+
+let Name = "Gaurav";
+let age = 21
+let isLogged = true;
+
+console.log(Name);
+console.log(age);
+console.log(isLogged);
+
+console.log(typeof "Gaurav");
+console.log(typeof 21); 
+console.log(typeof false);
+
+console.log(typeof undefined);
+console.log(typeof null);
