1e2ed284135617d22050e06e280e2ce38bc8a630
GSingh2432002 <gsingh332211@gmail.com>
2024-02-25 10:31:02 +0530

    Update

---
+++ 04.Comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined < null); 
+console.log(undefined <= null);
+console.log(undefined > null); 
+console.log(undefined == null); 
+
+console.log("2" === 2);
