06c34a61abfd92d294fd554f65237c5704c683f6
GSingh2432002 <gsingh332211@gmail.com>
2024-02-26 10:40:20 +0530

    Update

--- 10.Arrays.js
+++ 10.Arrays.js
@@ -21,12 +21,38 @@ const myArr = [0,1,2,3,4,5];
 
 //Slice -> In slice the original array remain same
 //Splice -> It changes the original array
-console.log("A", myArr);
-const myn1 = myArr.slice(1,3)
 
-console.log(myn1);
-console.log("B", myArr);
+// console.log("A", myArr);
+// const myn1 = myArr.slice(1,3)
 
-const myn2 = myArr.splice(1,3);
-console.log("C", myArr);
-console.log(myn2);
+// console.log(myn1);
+// console.log("B", myArr);
+
+// const myn2 = myArr.splice(1,3);
+// console.log("C", myArr);
+// console.log(myn2);
+
+const marvel_heros = ["Thor", "Ironman", "Superman", "Spiderman"];
+const  dc_heros = ["The Flash", "Green Lantern", "Wonder Woman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+// const all_Heros = marvel_heros.concat(dc_heros);
+// console.log(all_Heros);
+
+//Spread Operator
+const all_new_heros = [...marvel_heros, ...dc_heros];
+// console.log(all_new_heros);
+
+// const another_array = [1,2,3,[4,5,6], 7, [6,7,[8,9]]];
+// const real_another_array = another_array.flat(Infinity);
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Gaurav"));
+// console.log(Array.from("Gaurav Kumar Singh"));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
