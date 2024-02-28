21e85e24a2a48e36fa414e2268ef6dff752b2623
GSingh2432002 <gsingh332211@gmail.com>
2024-02-28 12:54:55 +0530

    Update

--- 11.Objects.js
+++ 11.Objects.js
@@ -36,45 +36,57 @@
 //Singleton
 //Object.create
 
-const tinderUser = {}
-tinderUser.id = "123abc"
-tinderUser.name = "Gaurav"
-tinderUser.isLoggedIn = false
+// const tinderUser = {}
+// tinderUser.id = "123abc"
+// tinderUser.name = "Gaurav"
+// tinderUser.isLoggedIn = false
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email : "gsingh332211@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"Gaurav",
+//             lastname:"Singh"
+//         }
+//     }
+// }
+// // console.log(regularUser.fullname?.userfullname);
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "c", 4: "d"}
+
+// // const obj3 = {obj1, obj2};  
+// // const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
+
+// const users = [
+//     {
+//         id:1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email: "h@gmail.com"
+//     },
+// ]
+
+// // users[1].email;
+
 // console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("id", "gift"));
 
-const regularUser = {
-    email : "gsingh332211@gmail.com",
-    fullname:{
-        userfullname:{
-            firstname:"Gaurav",
-            lastname:"Singh"
-        }
-    }
+//  ***************************** Part Three ************************************
+//Destructuring
+
+const course = {
+    coursename : "Js Hindi",
+    price : "999",
+    courseInstructor : "Gaurav"
 }
-// console.log(regularUser.fullname?.userfullname);
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "c", 4: "d"}
 
-// const obj3 = {obj1, obj2};  
-// const obj3 = Object.assign({}, obj1, obj2)
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
-
-const users = [
-    {
-        id:1,
-        email: "h@gmail.com"
-    },
-    {
-        id:1,
-        email: "h@gmail.com"
-    },
-]
-
-// users[1].email;
-
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("id", "gift"));
+const {courseInstructor} = course;
+console.log(courseInstructor);
